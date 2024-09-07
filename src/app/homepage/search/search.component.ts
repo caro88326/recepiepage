b16b8e268932fa63dfda9d7ecef4f8a34beb2339
Your Name <you@example.com>
2024-09-07 10:42:57 +0200

    copy filter and button

--- src/app/homepage/search/search.component.ts
+++ src/app/homepage/search/search.component.ts
@@ -1,24 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
 import { ButtonModule } from 'primeng/button';
 import { DialogModule } from 'primeng/dialog';
+import { ListboxModule } from 'primeng/listbox';
+import { FormsModule } from '@angular/forms';
 
 
-
+interface City {
+  name: string;
+  code: string;
+}
 
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [IconFieldModule, InputIconModule, ButtonModule, DialogModule],
+  imports: [IconFieldModule, InputIconModule, ButtonModule, DialogModule, ListboxModule, FormsModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
   visible: boolean = false;
 
   showDialog() {
       this.visible = true;
   }
+
+    cities!: City[];
+
+    selectedCity!: City;
+
+    ngOnInit() {
+        this.cities = [
+            { name: 'New York', code: 'NY' },
+            { name: 'Rome', code: 'RM' },
+            { name: 'London', code: 'LDN' },
+            { name: 'Istanbul', code: 'IST' },
+            { name: 'Paris', code: 'PRS' }
+        ];
+    }
 }
