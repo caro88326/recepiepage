3276bafb520e1d03205a19b345027b9d774265ed
Your Name <you@example.com>
2024-09-07 21:41:41 +0200

    search complete

--- src/app/homepage/search/search.component.ts
+++ src/app/homepage/search/search.component.ts
@@ -8,12 +8,13 @@ import { FormsModule } from '@angular/forms';
 import { RecepieService } from '../../recepie.service';
 import { RecepieInterface } from '../../interfaces/recepie-interface';
 import { NgFor } from '@angular/common';
+import { MultiSelectModule } from 'primeng/multiselect'; //Wenn nicht ausgewählt löschen!!
 
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [IconFieldModule, InputIconModule, ButtonModule, DialogModule, ListboxModule, FormsModule, NgFor],
+  imports: [IconFieldModule, InputIconModule, ButtonModule, DialogModule, ListboxModule, FormsModule, NgFor, MultiSelectModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
@@ -28,14 +29,6 @@ export class SearchComponent implements OnInit {
   recepieList : RecepieInterface []; 
   ingredients = {};
   
-
-  constructor () {
-    this.recepieList = this.recepieService.getAllRecepies();
-    this.ingredients = this.recepieService.getAllIngredients();
-    console.log(this.ingredients)
-  }
-
-
   tagN : string[] = [];
   selectedTagN! : string;
   tagE : string [] = [];
@@ -44,19 +37,39 @@ export class SearchComponent implements OnInit {
   selectedEat! : string;
   notEat : string [] = [];
   selectedNotEat! : string;
+  time : string [] = ['unter 30min', '30min bis 1h', 'über 1h']
+  selectedTime!: string;
+  tag : { pch : string; t: string[]; st: string; }[] = [];
 
-  tag = [{t: this.tagN, st : this.selectedTagN}, {t : this.tagE, st : this.selectedTagE},]
+  constructor () {
+    this.recepieList = this.recepieService.getAllRecepies();
+    this.ingredients = this.recepieService.getAllIngredients();
 
-    ngOnInit() {
-      for (var recepie of this.recepieList) {
-        const n = recepie.tagN
-        if (n != '' && this.tagN.includes(n) === false) {this.tagN.push(n!)}
-        const e = recepie.tagE
-        if (e != '' && this.tagE.includes(e) === false) {this.tagE.push(e!)}
-      }
-      // for (var rep of this.ingredients) {
+  }
+  ngOnInit() {
+    const ing : {rep: string, texture:string} []= Object.values(this.ingredients);
 
-      // }
+    for (var recepie of this.recepieList) {
+      const n = recepie.tagN
+      if (n != '' && this.tagN.includes(n) === false) {this.tagN.push(n!)}
+      const e = recepie.tagE
+      if (e != '' && this.tagE.includes(e) === false) {this.tagE.push(e!)}
+    }
+    for (var i of ing) {
+      const n = i.rep
+      if (n != '' && this.eat.includes(n) === false) {this.eat.push(n!)}
+      if (n != '' && this.notEat.includes(n) === false) {this.notEat.push('ohne ' + n!)}
+    }
+
+    this.tag = [{pch: 'Dauer', t: this.time, st: this.selectedTime}, {pch: 'Ernärung', t : this.tagE.sort(), st : this.selectedTagE}, {pch: 'Nationalität', t: this.tagN.sort(), st : this.selectedTagN}, {pch: 'Mit', t : this.eat.sort(), st : this.selectedEat}, {pch: 'Ohne', t : this.notEat.sort(), st : this.selectedNotEat}]
+    
   }
   
+
+  
+
+
+
+  
 }
+
